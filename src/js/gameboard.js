f27e99467e76b35f64bd9c2a0cc49926aa064d72
oscarnava <oscarnava@gmail.com>
2019-11-15 10:21:51 -0600

    Refactor Gameboard

--- src/js/gameboard.js
+++ src/js/gameboard.js
@@ -1,22 +1,27 @@
 import Ship from './ship';
-import * as globals from './globals';
 
 export default class Gameboard {
-  constructor() {
-    this.board = Array(globals.BOARD_SIZE * globals.BOARD_SIZE).fill('');
+  constructor(boardSize) {
+    this.boardSize = boardSize;
+    this.board = Array(this.boardSize * this.boardSize).fill('');
   }
 
   canPlaceShip(options) {
     const ship = new Ship(options);
     if (ship.x < 0 || ship.y < 0) return false;
 
-    return !ship.getCoordinates().find(([x, y]) => (x >= globals.BOARD_SIZE) || (y >= globals.BOARD_SIZE) || (this.board[x + globals.BOARD_SIZE * y] !== ''));
+    return !ship.getCoordinates().find(([x, y]) => (x >= this.boardSize) || (y >= this.boardSize) || (this.board[x + this.boardSize * y] !== ''));
   }
 
   placeShip(options) {
     const ship = new Ship(options);
     ship.getCoordinates().forEach(([x, y]) => {
-      this.board[x + globals.BOARD_SIZE * y] = ship.length;
+      this.board[x + this.boardSize * y] = ship;
     });
+    return ship;
+  }
+
+  receiveAttack (x, y) {
+
   }
 }
