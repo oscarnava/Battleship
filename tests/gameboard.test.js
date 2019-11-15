605ffd7a8a7416ece958f395958ab98cbca9ae6d
oscarnava <oscarnava@gmail.com>
2019-11-15 11:56:45 -0600

    Fixed and created tests

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -4,17 +4,15 @@ import Gameboard from '../src/js/gameboard';
 it('should place a ship at specific coordinate', () => {
   const gameboard = new Gameboard(config.BOARD_SIZE);
   const shipOptions = { length: config.SHIP_SIZES[0], pos: [0, 0] };
-  const expectedBoard = Array(config.BOARD_SIZE * config.BOARD_SIZE).fill('');
   const ship = gameboard.placeShip(shipOptions);
   expect(ship.isShip).toBe(true);
 
-  expectedBoard[0] = ship;
-  expectedBoard[1] = ship;
-  expectedBoard[2] = ship;
-  expectedBoard[3] = ship;
-  expectedBoard[4] = ship;
-  expect(gameboard.board).toEqual(expectedBoard);
-  expect(gameboard.board[10]).toBe('');
+  expect(gameboard.getCell(0, 0)).toEqual(ship);
+  expect(gameboard.getCell(1, 0)).toEqual(ship);
+  expect(gameboard.getCell(2, 0)).toEqual(ship);
+  expect(gameboard.getCell(3, 0)).toEqual(ship);
+  expect(gameboard.getCell(4, 0)).toEqual(ship);
+  expect(gameboard.getCell(0, 1)).not.toBe(ship);
 });
 
 it('should validate if a place is available for a ship', () => {
@@ -33,5 +31,17 @@ it('sould record an attack correcttly', () => {
   const ship = gameboard.placeShip({ length: 2, pos: [0, 0] });
 
   gameboard.receiveAttack(0, 0);
+
   expect(ship.damage).toEqual(1);
+  expect(gameboard.isHit(0, 0)).toBe(true);
+
+  gameboard.receiveAttack(0, 1);
+
+  expect(ship.damage).toEqual(1);
+  expect(gameboard.isMiss(0, 1)).toBe(true);
+
+  gameboard.receiveAttack(1, 0);
+
+  expect(ship.damage).toEqual(2);
+  expect(gameboard.isHit(1, 0)).toBe(true);
 });
