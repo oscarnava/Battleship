f8240e465467f1ff837b186260f92dc4e56c7705
oscarnava <oscarnava@gmail.com>
2019-11-20 07:44:04 -0600

    Fix linter errors

--- src/js/player.js
+++ src/js/player.js
@@ -5,7 +5,11 @@ export default class Player {
 
   // eslint-disable-next-line class-methods-use-this
   strategy(moves) {
-    const hit = (x, y) => (x >= 0) && (y >= 0) && (x < this.gameboard.boardSize) && (y < this.gameboard.boardSize) && this.gameboard.isHit(x, y);
+    const hit = (x, y) => (x >= 0)
+        && (y >= 0)
+        && (x < this.gameboard.boardSize)
+        && (y < this.gameboard.boardSize)
+        && this.gameboard.isHit(x, y);
 
     const target = moves.find(
       ({ x, y }) => hit(x - 1, y) || hit(x + 1, y) || hit(x, y - 1) || hit(x, y + 1),
