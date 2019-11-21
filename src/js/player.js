99e7b586c2849bdb880e6a41c53140b478c36f23
oscarnava <oscarnava@gmail.com>
2019-11-21 13:48:56 -0600

    Improve AI

--- src/js/player.js
+++ src/js/player.js
@@ -1,23 +1,79 @@
+import Ship from './ship';
+
+const logProbs = (probs) => {
+  const fmt = (n) => '0'.repeat(n < 16 ? 1 : 0) + n.toString(16);
+
+  console.clear();
+  for (let y = 0; y < probs.length; y += 1) {
+    console.log(probs[y].map(fmt).join('.'));
+  }
+};
+
+const evaluate = (ship, board, probs) => {
+  const coord = ship.coordinates;
+  // console.log(cord);
+
+  if (coord.find(
+    ([x, y]) => board.isMiss(x, y) || board.isSunk(x, y),
+  )) return;
+
+  const weight = coord.reduce(
+    (wgt, [x, y]) => wgt * (board.isHitOnly(x, y) ? 5 : 1),
+    1,
+  );
+
+  coord.forEach(([x, y]) => { if (!board.isHitOnly(x, y)) probs[y][x] += weight; });
+};
+
 export default class Player {
   constructor(gameboard) {
     this.gameboard = gameboard;
   }
 
+  testShipSize(shipLen, probs) {
+    const board = this.gameboard;
+    const size = board.boardSize;
+    const ship = new Ship({ length: shipLen, pos: [0, 0] });
+
+    for (let y = 0; y < size; y += 1) {
+      for (let x = 0; x < size - shipLen + 1; x += 1) {
+        evaluate(ship.moveTo(x, y), board, probs);
+      }
+    }
+
+    ship.turn(); // Vertical
+
+    for (let x = 0; x < size; x += 1) {
+      for (let y = 0; y < size - shipLen + 1; y += 1) {
+        evaluate(ship.moveTo(x, y), board, probs);
+      }
+    }
+
+    return probs;
+  }
+
   // eslint-disable-next-line class-methods-use-this
   strategy(moves) {
-    const hit = (x, y) => (x >= 0)
-        && (y >= 0)
-        && (x < this.gameboard.boardSize)
-        && (y < this.gameboard.boardSize)
-        && this.gameboard.isHit(x, y)
-        && !this.gameboard.isSunk(x, y);
+    const size = this.gameboard.boardSize;
+    const probs = Array.from({ length: size }, () => Array(size).fill(0));
+    const shipsLeft = this.gameboard.shipsLeft();
 
-    const target = moves.find(
-      ({ x, y }) => hit(x - 1, y) || hit(x + 1, y) || hit(x, y - 1) || hit(x, y + 1),
-    );
-    if (target) return target;
+    shipsLeft.forEach((shipSize) => {
+      this.testShipSize(shipSize, probs);
+    });
 
-    return moves[Math.floor(Math.random() * moves.length)];
+    // logProbs(probs);
+
+    for (let i = 0; i < moves.length - 1; i += 1) {
+      const j = Math.floor(Math.random() * moves.length);
+      if (i !== j) {
+        const tmp = moves[i];
+        moves[i] = moves[j];
+        moves[j] = tmp;
+      }
+    }
+
+    return moves.reduce((a, b) => (probs[a.y][a.x] > probs[b.y][b.x] ? a : b));
   }
 
   getMove() {
